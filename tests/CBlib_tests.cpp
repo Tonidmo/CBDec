e0ef83085545330c9e32db9388be130db81c8a3e
Imanol Etxezarreta <ietxezarretam@gmail.com>
2024-02-08 20:30:40 +0100

    Fix tests for new ClosedBranch class implementation.

--- tests/CBlib_tests.cpp
+++ tests/CBlib_tests.cpp
@@ -16,32 +16,43 @@
 
 #include "CBlib.h"
 
+#define  INITIAL_MEM_ALLOC   (uint16_t)UINT16_MAX
+
 /***********************************************************************************************************************
  * START OF CLOSEDBRANCH CLASS TESTS
  **********************************************************************************************************************/
-
 TEST(CBlib_CLOSED_BRANCH, 001_DEFAULT_CONSTRUCTOR) {
 
-   std::vector<bool> au1_aux_checks = std::vector<bool>(MAX_BRANCH_CHECKS_LEN, false);
-   std::vector<bool> au1_aux_events = std::vector<bool>(MAX_BRANCH_EVENTS_LEN, false);
+   // To initialize with
+   std::vector<bool> au1_aux_checks = std::vector<bool>(INITIAL_MEM_ALLOC, false);
+   std::vector<bool> au1_aux_events = std::vector<bool>(INITIAL_MEM_ALLOC, false);
+
+   // To compare with
+   std::vector<bool> au1_aux2_checks = std::vector<bool>(INITIAL_MEM_ALLOC, false);
+   std::vector<bool> au1_aux2_events = std::vector<bool>(INITIAL_MEM_ALLOC, false);
 
    CBlib::ClosedBranch o_cb = CBlib::ClosedBranch();
 
-   ASSERT_EQ(MAX_BRANCH_CHECKS_LEN, o_cb.get_cb_checks().size());
-   ASSERT_EQ(MAX_BRANCH_EVENTS_LEN, o_cb.get_cb_events().size());
-   ASSERT_EQ(au1_aux_checks, o_cb.get_cb_checks());
-   ASSERT_EQ(au1_aux_events, o_cb.get_cb_events());
+   au1_aux_checks.clear();
+   au1_aux_events.clear();
+
+   ASSERT_EQ(au1_aux_checks.empty(), true);
+   ASSERT_EQ(au1_aux_events.empty(), true);
+   ASSERT_EQ(INITIAL_MEM_ALLOC, o_cb.get_cb_checks().size());
+   ASSERT_EQ(INITIAL_MEM_ALLOC, o_cb.get_cb_events().size());
+   ASSERT_EQ(au1_aux2_checks, o_cb.get_cb_checks());
+   ASSERT_EQ(au1_aux2_events, o_cb.get_cb_events());
 }
 
 TEST(CBlib_CLOSED_BRANCH, 002_CONSTRUCTOR_1) {
 
-   std::vector<bool> au1_aux_checks = std::vector<bool>(MAX_BRANCH_CHECKS_LEN/2, false);
-   std::vector<bool> au1_aux_events = std::vector<bool>(MAX_BRANCH_EVENTS_LEN/2, false);
+   std::vector<bool> au1_aux_checks = std::vector<bool>(INITIAL_MEM_ALLOC/2, false);
+   std::vector<bool> au1_aux_events = std::vector<bool>(INITIAL_MEM_ALLOC/2, false);
 
    CBlib::ClosedBranch o_cb = CBlib::ClosedBranch(au1_aux_checks, au1_aux_events);
 
-   ASSERT_EQ(MAX_BRANCH_CHECKS_LEN/2, o_cb.get_cb_checks().size());
-   ASSERT_EQ(MAX_BRANCH_EVENTS_LEN/2, o_cb.get_cb_events().size());
+   ASSERT_EQ(INITIAL_MEM_ALLOC/2U, o_cb.get_cb_checks().size());
+   ASSERT_EQ(INITIAL_MEM_ALLOC/2U, o_cb.get_cb_events().size());
    ASSERT_EQ(au1_aux_checks, o_cb.get_cb_checks());
    ASSERT_EQ(au1_aux_events, o_cb.get_cb_events());
 }
